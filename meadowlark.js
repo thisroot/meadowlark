459fd6cb5a9642c4eb1b869f54908d83d2e37a61
Rey Bango <reybango@gmail.com>
2016-09-13 11:39:02 -0400

    Create fortune module and updated code to use it

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fortune = require('./lib/fortune.js');
 
 const app = express();
 
@@ -10,6 +11,9 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+// Setup the static route for assets
+app.use(express.static(__dirname + '/public'));
+
 // Main site page
 app.get('/', function(req, res){
   res.render('home');
@@ -17,7 +21,7 @@ app.get('/', function(req, res){
 
 // About page
 app.get('/about', function(req, res){
-  res.render('about');
+  res.render('about', { fortune : fortune.getFortune() });
 });
 
 // custom 404 page
