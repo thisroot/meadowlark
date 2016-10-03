87674ea732b914156d0c406aa48a815c8dd7ef49
Rey Bango <reybango@gmail.com>
2016-10-03 14:40:29 -0400

    Added HB helper to prepend static route for static assets

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt){
+
+	// load plugins
+	[
+		'grunt-cafe-mocha',
+		'grunt-contrib-jshint',
+		'grunt-contrib-less',
+		// 'grunt-contrib-uglify',
+		// 'grunt-contrib-cssmin',
+		// 'grunt-hashres',
+		// 'grunt-lint-pattern',
+	].forEach(function(task){
+		grunt.loadNpmTasks(task);
+	});
+
+	// configure plugins
+	grunt.initConfig({
+		cafemocha: {
+			all: { src: 'qa/tests-*.js', options: { ui: 'tdd' }, }
+		},
+		jshint: {
+			app: ['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js'],
+			qa: ['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'],
+		},
+		less: {
+			development: {
+				options: {
+					customFunctions: {
+						static: function(lessObject, name) {
+							return 'url("' +
+								require('./lib/static.js').map(name.value) +
+								'")';
+						}
+					}
+				},
+				files: {
+					'public/css/main.css': 'less/main.less'
+				}
+			}
+		}
+	});	
+
+	// register tasks
+	grunt.registerTask('default', ['cafemocha','jshint','exec', 'lint_pattern']);
+	grunt.registerTask('static', ['less', 'cssmin', 'uglify', 'hashres']);
+};
